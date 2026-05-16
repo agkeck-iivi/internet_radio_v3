910bf82174ce3fa80dfb472dda6b6082d2daf27b
Andy Keck <agkeck.iivi@gmail.com>
2026-05-16 12:07:31 -0600

    Change the default digital attenuation to -3db.  This works well with the tivoli audio device.

--- components/app_config/app_config.c
+++ components/app_config/app_config.c
@@ -8,7 +8,7 @@ static const char *NVS_NAMESPACE = "app_config";
 
 app_runtime_config_t g_runtime_config = {
     .analog_attenuation = PCM5122_ANALOG_ATTEN_6DB,
-    .digital_attenuation = PCM5122_DIGITAL_ATTEN_18DB,
+    .digital_attenuation = PCM5122_DIGITAL_ATTEN_3DB,
     .power_save_mode = POWER_SAVE_LIGHT_DEEP,
     .light_sleep_delay_ms = 20 * 60 * 1000,    // 20 minutes (prod default)
     .deep_sleep_delay_ms = 2 * 60 * 60 * 1000, // 2 hours
