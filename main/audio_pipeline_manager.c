5897fd517b00810db6ff6722b98061fd2ad1afcf
Andy Keck <agkeck.iivi@gmail.com>
2026-05-23 13:30:32 -0600

    Add 150ms delay for sending the audio_on ir remote command.

--- main/audio_pipeline_manager.c
+++ main/audio_pipeline_manager.c
@@ -40,6 +40,12 @@ const char *codec_type_to_string(codec_type_t codec) {
   }
 }
 
+void delayed_audio_on_task(void *pvParameters) {
+  vTaskDelay(pdMS_TO_TICKS(150));
+  ESP_ERROR_CHECK(ir_remote_turn_audio_on());
+  vTaskDelete(NULL);
+}
+
 static int _http_stream_event_handle(http_stream_event_msg_t *msg) {
   switch (msg->event_id) {
   case HTTP_STREAM_RESOLVE_ALL_TRACKS:
@@ -85,7 +91,10 @@ static esp_err_t codec_event_cb(audio_element_handle_t el,
       extern bool get_mute_state(void);
       if (g_runtime_config.ir_protocol != IR_PROTOCOL_NONE) {
         ESP_LOGI(TAG, "Audio stream active. Ensuring Bose Audio is ON.");
-        ir_remote_turn_audio_on();
+        // ir_remote_turn_audio_on();
+        // send another ir command after a delay
+        xTaskCreate(delayed_audio_on_task, "delayed_audio_on_task", 3*1024, NULL,
+                    tskIDLE_PRIORITY + 1, NULL);
       }
     }
   }
